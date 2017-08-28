cadd42e8d04db924772963bde13e26d0631eb044
CommandCraft <e.lancha98@gmail.com>
2017-08-28 09:05:51 +0200

    Initial commit

---
+++ spec/jutge-for-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JutgeForAtomView from '../lib/jutge-for-atom-view';
+
+describe('JutgeForAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
